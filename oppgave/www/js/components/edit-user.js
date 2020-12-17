4b6827a76cd03101cb9264c7ad173bd57e4dcf35
done-juseyo <76155775+done-juseyo@users.noreply.github.com>
2020-12-17 10:49:13 +0100

    Update edit-user.js

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -7,7 +7,21 @@ class EditUser extends LitElement {
     };
   }
 
-  // din kode her
-
+  //updates a user's info
+  updateUser(e) {
+    const dataForm = new FormData(e.target.form);
+    console.log(e)
+    fetch('api/updateUser.php', {
+     method: 'POST',
+     body: dataForm
+    }).then(res=>res.json())
+      .then(data=>{
+        if (data.status=='success') {
+            console.log("The user was successfully updated");
+        } else {
+            console.log("Error: The user was not updated");
+        }
+      })
+  }
 }
 customElements.define('edit-user', EditUser);
