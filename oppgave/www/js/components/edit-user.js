c5b1bc9f6d570eb6a3b11728c6186fd03c4a5175
done-juseyo <76155775+done-juseyo@users.noreply.github.com>
2020-12-17 11:21:05 +0100

    Update edit-user.js

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -6,8 +6,39 @@ class EditUser extends LitElement {
       user: { type: Object }
     };
   }
+  
+  render () {
+    return html`
+    <head>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    </head>
+    <form onsubmit="javascript: return false;" id="userForm" method="POST">
+      <div class="form-group pt-5 ml-3" style="width: 30rem;">
+        <label for="email">Email</label>
+        <input class="form-control" id="uname" name="uname" type="text" value="${this.user.uname}" required>
+        <input type="hidden" id="uid" name="uid" value="${this.user.uid}">
+      </div>
+      <div class="form-group pt-1 ml-3" style="width: 30rem;">
+        <label for="firstName">First Name</label>
+        <input class="form-control" id="firstName" name="firstName" type="text" value="${this.user.firstName}" required>
+      </div>
+      <div class="form-group pt-1 ml-3" style="width: 30rem;">
+        <label for="lastName">Last Name</label>
+        <input class="form-control" id="lastName" name="lastName" type="text" value="${this.user.lastName}" required>
+      </div>
+      <div class="form-group pt-1 ml-3" style="width: 30rem;">
+        <label for="oldpassword">Old Password</label>
+        <input type="password" class="form-control" id="oldpassword" name="oldpassword" type="text" value="">
+      </div>
+      <div class="form-group pt-1 ml-3" style="width: 30rem;">
+        <label for="newpassword">New Password</label>
+        <input type="password" class="form-control" id="password" name="password" type="text" value="">
+      </div>
+      <input type="submit" @click=${this.updateUser} id="submitForm" name="editUser" class="btn btn-warning mt-2 ml-3" value="Edit User"></input>
+    </form>
+    `;
+  }
 
-  //updates a user's info
   updateUser(e) {
     const dataForm = new FormData(e.target.form);
     console.log(e)
